a30e7a63ac712abf890f57e816438bc443918403
Jaime Piña <variadico@users.noreply.github.com>
2020-10-18 20:30:45 -0700

    Increase depth limit

--- formatter.go
+++ formatter.go
@@ -93,7 +93,7 @@ type visit struct {
 }
 
 func (p *printer) printValue(v reflect.Value, showType, quote bool) {
-	if p.depth > 10 {
+	if p.depth > 100 {
 		io.WriteString(p, "!%v(DEPTH EXCEEDED)")
 		return
 	}
